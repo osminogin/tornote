308bc2c80b872934e1bc08f940c95d7955fe0ffb
Vladimir Osintsev <osintsev@gmail.com>
2016-06-13 15:03:09 +0300

    Initial tests added

--- handlers_test.go
+++ handlers_test.go
@@ -4,12 +4,19 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func TestFrontPage(t *testing.T) {
-	//req, _ := http.NewRequest("GET", "", nil)
 	w := httptest.NewRecorder()
+	r := mux.NewRouter()
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	r.HandleFunc("/", frontPageHandler).Methods("GET")
+	r.ServeHTTP(w, req)
+
 	if w.Code != http.StatusOK {
-		t.Errorf("Home page didn't return %v", http.StatusOK)
+		t.Errorf("Frontpage return code %v instead %v", w.Code, http.StatusOK)
 	}
 }
